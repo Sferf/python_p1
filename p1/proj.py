3855474bab69d9ac1f7bb68fa10c634f5d3d557f
Sferf <Isach.Pasha@yandex.by>
2022-04-18 04:02:14 +0300

    need statistics only

--- p1/proj.py
+++ p1/proj.py
@@ -1,8 +1,6 @@
 import pygame
 import tkinter as tk
 import time
-#import keyboard
-
 
 default_font_name = "Arial Bold"
 default_font_size = "13"
@@ -13,14 +11,44 @@ default_start_message = "Это клавиатурный тренажер"
 default_button_message = "Нажмите для начала"
 default_title_message = "Клавиатурный тренажер"
 default_music_for_traine = "./music.mp3"
-default_ready_message = "READY ...????" 
-default_steady_message = "STEADY......" 
-default_go_message = "GO!!!"
-default_ready_time = 11
-default_steady_time = 9
-default_go_time = 2
+default_ready_message = "READY...????" 
+default_steady_message = "STEADY......zzz" 
+default_go_message = "GO!!!(^_^)"
+default_ready_time = 8
+default_steady_time = 11
+default_go_time = 2.3
+funny_mode_value = "on"
 
 
+def print_ready_steady_go_message(message, message_time):
+    my_message = tk.Label(text = message, font = default_font_settings)
+    my_message.pack()
+    window.update()
+    time.sleep(message_time)
+    my_message.destroy()
+    window.update()
+
+
+key_number = None
+key_need_to_pres = None
+block = None
+def continue_key(entry = None):
+    #print(entry)
+    global key_number
+    key_number += 1
+    if entry.char == key_need_to_pres:
+        global block
+        block = False
+
+def funny_mode():
+    pygame.init()
+    pygame.mixer.init()
+    pygame.mixer.music.load(default_music_for_traine)
+    pygame.mixer.music.play()
+
+    print_ready_steady_go_message(default_ready_message, default_ready_time)
+    print_ready_steady_go_message(default_steady_message, default_steady_time)
+    print_ready_steady_go_message(default_go_message, default_go_time)
 
 def start_of_training():
     start_button.destroy()
@@ -28,31 +56,8 @@ def start_of_training():
     window.update()
     file = (open(default_source_file_name, 'r').read())
 
-    pygame.init()
-    pygame.mixer.init()
-    pygame.mixer.music.load(default_music_for_traine)
-    pygame.mixer.music.play()
-
-    ready = tk.Label(text = default_ready_message, font = default_font_settings)
-    ready.pack()
-    window.update()
-    time.sleep(default_ready_time)
-    ready.destroy()
-    window.update()
-
-    steady = tk.Label(text = default_steady_message, font = default_font_settings)
-    steady.pack()
-    window.update()
-    time.sleep(default_steady_time)
-    steady.destroy()
-    window.update()
-
-    go = tk.Label(text = default_go_message, font = default_font_settings)
-    go.pack()
-    window.update()
-    time.sleep(default_go_time)
-    go.destroy()
-    window.update()
+    if funny_mode_value == "on":
+        funny_mode()
 
     for i in range(len(file) // default_block_size):
         left_border = i * default_block_size
@@ -61,14 +66,24 @@ def start_of_training():
         text = tk.Label(text = substring, font = default_font_settings)
         text.pack()
 
+        result_string = ""
+        global key_number
+        key_number = 0
         for j in substring:
-            t = 0
-            while t < 1000:
-            #not keyboard.is_pressed(j):
-                t += 1
-                time.sleep(0.01)
-                print(t)
+            result_message = tk.Label(text = result_string, font = default_font_settings)
+            result_message.pack()
+            global key_need_to_pres
+            key_need_to_pres = j
+            global block
+            block = True
+            while block:
+                window.bind_all("<KeyPress>", continue_key)
+                statistics = tk.Label(text = key_number, font = default_font_settings)
+                statistics.pack()
                 window.update()
+                statistics.destroy()
+            result_string += j
+            result_message.destroy()
         text.destroy()
     
 
